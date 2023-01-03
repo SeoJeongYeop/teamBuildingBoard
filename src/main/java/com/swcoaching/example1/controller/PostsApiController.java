2721fa4545ea0b64df46b649a10afbb0809b49fe
SeoJeongYeop <sjyswe99@gmail.com>
2023-01-04 01:53:30 +0900

    feat: Add 등록/조회/수정 API

---
+++ src/main/java/com/swcoaching/example1/controller/PostsApiController.java
@@ -0,0 +1,29 @@
+package com.swcoaching.example1.controller;
+
+import com.swcoaching.example1.controller.dto.PostsResponseDto;
+import com.swcoaching.example1.controller.dto.PostsSaveRequestDto;
+import com.swcoaching.example1.controller.dto.PostsUpdateRequestDto;
+import com.swcoaching.example1.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PostMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
---
+++ src/main/java/com/swcoaching/example1/controller/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.swcoaching.example1.controller.dto;
+
+import com.swcoaching.example1.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
