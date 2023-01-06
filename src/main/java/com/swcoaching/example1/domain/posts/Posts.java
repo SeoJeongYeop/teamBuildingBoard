9bec49645d1cd3fe9f03f4b3cb667a4616d270b2
SeoJeongYeop <sjyswe99@gmail.com>
2023-01-07 02:00:07 +0900

    feat: JPA Auditing을 이용한 등록/수정 시간 자동화 추가

--- src/main/java/com/swcoaching/example1/domain/posts/Posts.java
+++ src/main/java/com/swcoaching/example1/domain/posts/Posts.java
@@ -1,6 +1,7 @@
 package com.swcoaching.example1.domain.posts;
 
 
+import com.swcoaching.example1.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
